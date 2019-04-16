827f4e2ec9f6bc75264f5c482252490d6c5d1a93
nixuebing <nixuebing@tvflnet.com>
2019-04-16 10:26:50 +0800

    项目初始

---
+++ src/main/java/com/ice/notes/code/NotesCodeApplication.java
@@ -0,0 +1,17 @@
+package com.ice.notes.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author : ACE
+ * @date : 2019/4/16
+ */
+@SpringBootApplication
+public class NotesCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesCodeApplication.class, args);
+    }
+
+}
