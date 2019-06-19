95e0d92e5a269c94e2bf89b61ba77a6e0c396988
nixuebing <nixuebing@tvflnet.com>
2019-06-19 18:11:33 +0800

    并发

--- src/main/java/com/ice/notes/code/concurrency/BlockedQueue.java
+++ src/main/java/com/ice/notes/code/concurrency/BlockedQueue.java
@@ -2,9 +2,7 @@ package com.ice.notes.code.concurrency;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.*;
 
 public class BlockedQueue<T> {
 
@@ -16,6 +14,9 @@ public class BlockedQueue<T> {
 
     private List<T> list = new ArrayList<>(10);
 
+    final StampedLock sl = new StampedLock();
+
+
     public void enq(T x) {
         lock.lock();
         try {
@@ -23,11 +24,41 @@ public class BlockedQueue<T> {
                 notFull.await();
             }
             notEmpty.signalAll();
-        }
-        catch (InterruptedException e){
+        } catch (InterruptedException e) {
 
-        }finally {
+        } finally {
             lock.unlock();
         }
+    }
+
+    public static void main(String[] args) {
+        try {
+            final StampedLock lock = new StampedLock();
+
+            Thread T1 = new Thread(() -> {
+                // 获取写锁
+                lock.writeLock();
+                // 永远阻塞在此处，不释放写锁
+                LockSupport.park();
+            });
+            T1.start();
+            // 保证 T1 获取写锁
+            Thread.sleep(100);
+            Thread T2 = new Thread(() ->
+                    // 阻塞在悲观读锁
+                    lock.readLock()
+            );
+            T2.start();
+            // 保证 T2 阻塞在读锁
+            Thread.sleep(100);
+            // 中断线程 T2
+            // 会导致线程 T2 所在 CPU 飙升
+            T2.interrupt();
+            T2.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
